88ffda5c370c0a3a14ea31990c137a2682c52299
CarlaMQ <carlaquintanar1907@gmail.com>
2025-09-15 21:20:26 +0900

    first application

--- server.js
+++ server.js
@@ -6,15 +6,29 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
 /* ***********************
- * Routes
+ * View Engine and Templates
  *************************/
-app.use(static)
 
+
+
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+/* ***********************
+ * Routes
+ *************************////Notice that instead of router.use, it is now app.use, meaning that the application itself will use this resource.
+app.use(static)
+//index route
+app.get("/", function (req, res) {
+  res.render("index", { title: "Home" })
+})
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
@@ -28,3 +42,12 @@ const host = process.env.HOST
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+
+
+//ROUTES EXPLANATION
+//app.get - The express application will watch the "get" object, within the HTTP Request, for a particular route.
+//"/" - This is route being watched. It indicates the base route of the application or the route which has no specific resource requested.
+//function(req, res){ - A JavaScript function that takes the request and response objects as parameters.
+//res.render() - The "res" is the response object, while "render()" is an Express function that will retrieve the specified view - "index" - to be sent back to the browser.
+//{title: "Home" } - The curly braces are an object (treated like a variable), which holds a name - value pair. This object supplies the value that the "head" partial file expects to receive. The object is passed to the view.
+//}) - The right curly brace closes the function, while the right parentheses closes the "get" route

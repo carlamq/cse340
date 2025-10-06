5e184b4b70f1b86bec3bcccf2772429f6d0d04d5
CarlaMQ <carlaquintanar1907@gmail.com>
2025-10-06 21:53:41 +0900

    learning act week 4

--- server.js
+++ server.js
@@ -13,13 +13,37 @@ const static = require("./routes/static")
 const utilities = require("./utilities/")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
+const session = require("express-session")
+const pool = require('./database/')
+const accountRoute = require("./routes/accountRoute")
+const bodyParser = require("body-parser")
+
+/* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({ //app.use() applies whatever is being invoked throughout the entire application.
+  store: new (require('connect-pg-simple')(session))({//creating a new session table in our PostgreSQL database using the "connect-pg-simple" package
+    createTableIfMissing: true,//tells the session to create a "session" table in the database if it does not already exist.
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,//indicates a "secret" name - value pair that will be used to protect the session. will be in .env
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+ }))
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 /* ***********************
  * View Engine and Templates
  *************************/
-
-
-
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
@@ -33,6 +57,12 @@ app.get("/", utilities.handleErrors(baseController.buildHome))
 
 // Inventory routes
 app.use("/inv", inventoryRoute)
+
+// Inventory routes
+app.use("/inv", inventoryRoute)
+
+// Account routes  
+app.use("/account", accountRoute)
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {

24b52b6ce3ad6cfd1a8ddfebc6102dc84474d46b
CarlaMQ <carlaquintanar1907@gmail.com>
2025-10-07 22:49:48 +0900

    routes order fixed

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -5,20 +5,23 @@ const invController = require("../controllers/invController")
 const utilities = require("../utilities/")
 const invValidate = require("../utilities/inventory-validation")
 
-// Route to build inventory by classification view
-router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
-
-// Route to build inventory detail view
-router.get("/detail/:invId", utilities.handleErrors(invController.buildByInvId));
-
-// Route to trigger intentional 500 error for testing
-router.get("/trigger-error", utilities.handleErrors(invController.triggerError));
-
 // Route to build inventory management view
 router.get("/", utilities.handleErrors(invController.buildManagementView));// the / is /inv/ from the url
 
 // Route to build add classification view
 router.get("/add-classification", utilities.handleErrors(invController.buildAddClassification));
+
+//Route to build add inventory
+router.get("/add-inventory", utilities.handleErrors(invController.buildAddInventory));
+
+// Route to trigger intentional 500 error for testing
+router.get("/trigger-error", utilities.handleErrors(invController.triggerError));
+
+// Route to build inventory by classification view (DEBE IR DESPUÉS de las rutas estáticas)
+router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
+
+// Route to build inventory detail view
+router.get("/detail/:invId", utilities.handleErrors(invController.buildByInvId));
 
 // Route to process add classification
 router.post("/add-classification", 
